f40be3ca6d5156693c01b09585a1714c3ab283e6
Analia <analuciaotano@hotmail.com>
2023-09-21 22:47:21 -0300

    commit

---
+++ javascript.js
@@ -0,0 +1,19 @@
+
+
+var nombre = prompt("Hola! Bienvenido/a a mi página web, por favor ingresá tu nombre:");
+
+console.log(nombre);
+
+alert("Hola: " + nombre);
+
+
+
+
+document.getElementById('comentarios').addEventListener('button'), (event) =>{
+    event.preventDefault(
+        
+    )
+}
+
+
+
